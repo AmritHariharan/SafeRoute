ac701d701a6ae00ea2e1bdb85cd738e4aa5f90c8
UnknownArcher <stuka241@gmail.com>
2016-10-09 03:02:38 -0400

    Added working warning indicators

--- src/js/app.js
+++ src/js/app.js
@@ -99,6 +99,84 @@ function processPosition(position) {
     badness_at_point(flipped, updateSafetyState);
 }
 
+
+// MONITORING CODE
+
+var vdata = gm.info.watchVehicleData(
+	processData,
+	function(){},
+	// Fuel
+	['EV_max_range',
+	'fuel_level',
+	// Tire Pressure
+	'tire_right_front_pressure',
+	'tire_left_front_pressure',
+	'tire_right_rear_pressure',
+	'tire_left_rear_pressure',
+	// Lights
+	'bulb_center_fail',
+	'bulb_frontright_turn_fail',
+	'bulb_frontleft_turn_fail'],
+	1000);
+
+var EV_max_range;
+var fuel_level;
+var tire_right_front_pressure;
+var tire_left_front_pressure;
+var tire_right_rear_pressure;
+var tire_left_rear_pressure;
+var bulb_center_fail;
+var bulb_frontright_turn_fail;
+var bulb_frontleft_turn_fail;
+
+
+function processData(data) {
+
+	// Setting variables
+	EV_max_range = data.EV_max_range;
+	fuel_level = data.fuel_level;
+	tire_right_front_pressure = data.tire_right_front_pressure;
+	tire_left_front_pressure = data.tire_left_front_pressure;
+	tire_right_rear_pressure = data.tire_right_rear_pressure;
+	tire_left_rear_pressure = data.tire_left_rear_pressure;
+	bulb_center_fail = data.bulb_center_fail;
+	bulb_frontright_turn_fail = data.bulb_frontright_turn_fail;
+	bulb_frontleft_turn_fail = data.bulb_frontleft_turn_fail;
+
+
+	console.log(EV_max_range);
+	// Fuel
+	if ((EV_max_range < 10 && EV_max_range != null) || (fuel_level < 10 && EV_max_range != null)) { // km
+		var element = document.getElementById('alertBoxFuel');
+		element.style.opacity = "1";
+	} else {
+		var element = document.getElementById('alertBoxFuel');
+		element.style.opacity = "0";
+	}
+
+console.log(tire_right_front_pressure);
+	// Tires
+	if (tire_right_front_pressure < 138 || tire_left_front_pressure < 138 || tire_right_rear_pressure < 138 || tire_left_rear_pressure < 138) { // kPaG
+		var element = document.getElementById('alertBoxTires');
+		element.style.opacity = "1";
+	} else {
+		var element = document.getElementById('alertBoxTires');
+		element.style.opacity = "0";
+	}
+
+	console.log(bulb_center_fail);
+	// Lights
+	if (bulb_center_fail == 1 || bulb_frontright_turn_fail == 1 || bulb_frontleft_turn_fail == 1) { // 1/0
+		var element = document.getElementById('alertBoxLights');
+		element.style.opacity = "1";
+	} else {
+		var element = document.getElementById('alertBoxLights');
+		element.style.opacity = "0";
+	}
+}
+
+// GO BACK TO THE MASONIC TEMPLE THEATRE
+
 function resetButton() {
     marker.setPosition( new google.maps.LatLng(42.340473, -83.062516) );
   	map.panTo( new google.maps.LatLng(42.340473, -83.062516) );
@@ -109,8 +187,8 @@ var i = 0;
 // Coordinates for points that map the demo journey
 var lat_arr = [42.340473, 42.340153, 42.342247, 42.343856, 42.346077, 42.347829, 42.349248, 42.351452, 42.353133, 42.354885, 42.357208, 42.358334, 42.360046, 42.361537, 42.362774, 42.364034, 42.365548, 42.366634, 42.368521, 42.370177, 42.372096, 42.373110, 42.373816, 42.374743, 42.374949, 42.374442, 42.373911, 42.373934, 42.374488];
 var lng_arr = [-83.062516, -83.066138, -83.067490, -83.068251, -83.069238, -83.070075, -83.070719, -83.071945, -83.072964, -83.074005, -83.074767, -83.074938, -83.074144, -83.072353, -83.070389, -83.067975, -83.064767, -83.062010, -83.058030, -83.055540, -83.052096, -83.049296, -83.046968, -83.046453, -83.047601, -83.049210, -83.050519, -83.049243, -83.047438];
-console.log("arr 1 length: " + lat_arr.length);
-console.log("arr 2 length: " + lng_arr.length);
+//console.log("arr 1 length: " + lat_arr.length);
+//console.log("arr 2 length: " + lng_arr.length);
 
 var interval
 
